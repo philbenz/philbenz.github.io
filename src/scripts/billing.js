40b4aed4c392a6b6be72954b59e065ae19b6051b
philbenz@gmail.com <philbenz@Morrhysseys-MacBook-Air.local>
2016-07-26 19:45:52 -0600

    updates to billing info js and html

--- src/scripts/billing.js
+++ src/scripts/billing.js
@@ -1,3 +1,41 @@
-$(function () {
-  console.log('hello!');
-})
+$(document).on('ready', function() {
+    //prevent the event default
+    // event.preventDefault();
+
+    //sanity check
+    console.log('sanity check with billing js');
+
+    //jQuery for the movie title
+    $('button').click('submit', function( event ) {
+      //ensure that the submit doesn't go to a server
+      event.preventDefault();
+
+
+    });
+
+    //ensure that shipping copies to billing
+    //ensure that when uncheck billing is left blank
+    $("#isSameAsShipping").click(function(){
+      if(this.checked) {
+        //populate billing info fields
+        $('.billingFirstName').val($('.shippingFirstName').val());
+        $('.billingLastName').val($('.shippingLastName').val());
+        $('.billingCompany').val($('.shippingCompany').val());
+        $('.billingAddressLine1').val($('.shippingAddressLine1').val());
+        $('.billingAddressLine2').val($('.shippingAddressLine2').val());
+        $('.billingCity').val($('.shippingCity').val());
+        $('.billingState').val($('.shippingState').val());
+        $('.billingZip').val($('.shippingZip').val());
+      } else {
+        //clear all billing info fields
+        $('.billingFirstName').val('');
+        $('.billingLastName').val('');
+        $('.billingCompany').val('');
+        $('.billingAddressLine1').val('');
+        $('.billingAddressLine2').val('');
+        $('.billingCity').val('');
+        $('.billingState').val('');
+        $('.billingZip').val('');
+      }
+    });
+  });
