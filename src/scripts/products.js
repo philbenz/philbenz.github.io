c95f036dcc1e377c026f6b7f838ae294ab56e82f
AustinMahan <amahan0424@gmail.com>
2016-07-27 18:33:44 -0600

    added feature

--- src/scripts/products.js
+++ src/scripts/products.js
@@ -5,20 +5,29 @@ $(function () {
     method: 'GET'
   }))
   .then(function(data){
-    var imgs = ['https://s-media-cache-ak0.pinimg.com/236x/f7/de/ed/f7deedb246866ed4b0b2aab9bd3b7676.jpg','http://2.bp.blogspot.com/-u66toyzKzUo/VCStwUNksjI/AAAAAAAABH0/h2WAJYGiasY/s1600/Rainbow%2Blighted%2Bping%2Bpong%2Btable.jpg','http://assets.nydailynews.com/polopoly_fs/1.2155594.1426789136!/img/httpImage/image.jpg_gen/derivatives/landscape_1200/glass-ping-pong-table.jpg','http://slippedydodah.com/images/table/Poly%20Pong%204way-ping%20pong.jpg','https://www.iwantmoretoys.com/images/products/floating-pool-ping-pong-table.jpg','http://www.toxel.com/wp-content/uploads/2009/08/pingpong02.jpg','https://playenthusiast.files.wordpress.com/2011/06/table-tennis-003.jpg','http://odditymall.com/includes/content/table-tennis-door-thumb.jpg','http://www.earthlymission.com/wp-content/uploads/2015/03/circular-ping-pong-table-1.jpg','https://metrouk2.files.wordpress.com/2016/07/ad213894936pic_shows_ping_.jpg','http://static4.businessinsider.com/image/4fb129d3eab8ea9c09000002-900/were-not-sure-if-this-is-just-a-regular-table-or-if-its-a-six-person-ping-pong-table.jpg','http://67.media.tumblr.com/50df10f236e47183738231880d5f4c12/tumblr_mx65t2QSjW1rouua1o6_1280.jpg', 'http://tabletennisnation.com/wp-content/uploads/2013/09/Full-table+Medium.jpg']
+
+    var imgs = ['https://s-media-cache-ak0.pinimg.com/236x/f7/de/ed/f7deedb246866ed4b0b2aab9bd3b7676.jpg','http://2.bp.blogspot.com/-u66toyzKzUo/VCStwUNksjI/AAAAAAAABH0/h2WAJYGiasY/s1600/Rainbow%2Blighted%2Bping%2Bpong%2Btable.jpg','http://assets.nydailynews.com/polopoly_fs/1.2155594.1426789136!/img/httpImage/image.jpg_gen/derivatives/landscape_1200/glass-ping-pong-table.jpg','http://slippedydodah.com/images/table/Poly%20Pong%204way-ping%20pong.jpg','https://www.iwantmoretoys.com/images/products/floating-pool-ping-pong-table.jpg','http://www.toxel.com/wp-content/uploads/2009/08/pingpong02.jpg','https://playenthusiast.files.wordpress.com/2011/06/table-tennis-003.jpg','http://odditymall.com/includes/content/table-tennis-door-thumb.jpg','http://www.earthlymission.com/wp-content/uploads/2015/03/circular-ping-pong-table-1.jpg','https://metrouk2.files.wordpress.com/2016/07/ad213894936pic_shows_ping_.jpg','http://static4.businessinsider.com/image/4fb129d3eab8ea9c09000002-900/were-not-sure-if-this-is-just-a-regular-table-or-if-its-a-six-person-ping-pong-table.jpg','http://67.media.tumblr.com/50df10f236e47183738231880d5f4c12/tumblr_mx65t2QSjW1rouua1o6_1280.jpg', 'http://tabletennisnation.com/wp-content/uploads/2013/09/Full-table+Medium.jpg', 'http://tabletennisnation.com/wp-content/uploads/2012/01/52ac18c78972137508a9a48dd702df84-677x385.jpg']
+    var Names = ['Tube Table','Glowing Table','Glass Table', 'WTF Table', 'Pool Table', 'Wall Table', 'Figure 8', 'Door Table', 'Round Table', 'Car Table', 'Pie Table', 'Multi Table', 'Wing Table', 'WhoDoesThis Table']
     console.log(imgs.length);
     var products = []
     var myData = data
     console.log(myData);
     for(var i = 0; i < data.length; i++){
-      products.push('<div class="productDiv" id="product' + i + '"><img src="' + imgs[i] +'"/> <p>' + data[i].description + '</p><br><p class="valueOf">' + data[i].price + '</p>')
+      products.push('<div class="productDiv text-center" id="product' + i + '"><img class="austinImg" src="' + imgs[i] +'"/><h3>' + Names[i] + '</h3> <p>' + data[i].description + '</p><p class="valueOf">' + data[i].price + '</p><div class="rating"><div class="bar b1"></div><div class="bar b2"></div><div class="bar b3"></div><div class="bar b4"></div><div class="bar b1"></div></div><div class="AddCart">Add to Cart</div>')
       $('.productHolder').append(products[i])
       loadLow()
+      barHover()
+      DivHover()
+      AddCartNum()
     }
     var allProducts = $('.productDiv')
-    console.log(allProducts);
+console.log(products);
+    function sortNumber(a,b) {
+      return a - b;
+    }
 
     function loadLow(){
+
       var arrLowHigh = []
       var objLowHigh = {}
       data.forEach(function(obj){
@@ -29,7 +38,8 @@ $(function () {
       for(var i = 0; i < arrLowHigh.length; i++){
         objLowHigh[arrLowHigh[i]] = i+1
       }
-      arrLowHigh.sort()
+      arrLowHigh = arrLowHigh.sort(sortNumber)
+      console.log(arrLowHigh);
       arrLowHigh.forEach(function(price){
         $('.productHolder').append(products[objLowHigh[price]-1])
       })
@@ -41,6 +51,9 @@ $(function () {
       for(var i = 0; i < arrhigLow.length; i++){
         $('.productHolder').prepend(arrhigLow[i])
       }
+      barHover()
+      DivHover()
+      AddCartNum()
     })
 
     $('#filtereds').change(function(){
@@ -48,9 +61,11 @@ $(function () {
       var secNum = parseFloat($('input[name=filtered]:radio:checked').val().split('-')[1])
       var products = []
       var highOrlow = {}
+      console.log(firstNum);
+      console.log(secNum);
       for(var i = 0; i < allProducts.length; i++){
-        if(firstNum < parseFloat(allProducts[i].lastChild.textContent.substr(1)) && parseFloat(allProducts[i].lastChild.textContent.substr(1)) < secNum ){
-          console.log(parseFloat(allProducts[i].lastChild.textContent.substr(1)));
+        console.log();
+        if(firstNum < parseFloat($(allProducts[i]).children()[3].textContent.substr(1)) && parseFloat($(allProducts[i]).children()[3].textContent.substr(1)) < secNum ){
           products.push(allProducts[i])
         }
       }
@@ -62,4 +77,28 @@ $(function () {
 
     })
   })
+  function barHover(){
+    $('.bar').hover(function(){
+      console.log('test');
+      $(this).css('background-color', 'black')
+      $(this).prevAll().css('background-color', 'black')
+      $(this).nextAll().css('background-color', 'white')
+    })
+  }
+  function DivHover(){
+    $('.productDiv').hover(function(){
+      $(this).children().last().fadeIn()
+    }, function(){
+      $(this).children().last().fadeOut()
+    })
+  }
+  function AddCartNum(){
+    $('.AddCart').click(function(){
+      console.log($(this));
+      $(this).fadeOut(100).fadeIn(10)
+
+      // .delay(200).css('background-color', 'limegreen')
+      $('.badge').text(parseInt($('.badge').text())+1)
+    })
+  }
 })
