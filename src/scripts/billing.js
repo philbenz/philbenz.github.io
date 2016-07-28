5424ff2e7d0216d9af6de2ac8dcce8628b07d621
philbenz@gmail.com <philbenz@Morrhysseys-MacBook-Air.local>
2016-07-28 09:12:32 -0600

    including stripe functionality and some basic crappy looking bootstrap

--- src/scripts/billing.js
+++ src/scripts/billing.js
@@ -1,21 +1,70 @@
 $(document).on('ready', function() {
     //prevent the event default
-    // event.preventDefault();
+    //event.preventDefault();
 
     //sanity check
     console.log('sanity check with billing js');
+    //[[cost, quantity, ]]
+    // var shoppingItems = [[]]
+
+    //Stripe Public Keys
+    //  test publishable: pk_test_hs0sBTr45jm1KOmFEE4bDAOU
+    //  test secret: sk_test_C6yqB06zrJzrwQRDNDFVtl8T
 
     buildStatesSelector();
 
     checkShoppingCart();
 
-    //jQuery for the movie title
-    $('button').click('submit', function( event ) {
+
+    $('#purchaseButton').click('submit', function( event ) {
       //ensure that the submit doesn't go to a server
       event.preventDefault();
 
+      var error = false;
 
-    });
+      // Get the values:
+      var ccNum = $('.creditCardNumber').val(),
+          cvcNum = $('.cvc').val(),
+          expMonth = $('.expMonth').val(),
+          expYear = $('.expYear').val();
+
+      // Validate the number:
+      if (!Stripe.card.validateCardNumber(ccNum)) {
+          error = true;
+          reportError('The credit card number appears to be invalid.');
+      }
+
+      // Validate the CVC:
+      if (!Stripe.card.validateCVC(cvcNum)) {
+          error = true;
+          reportError('The CVC number appears to be invalid.');
+      }
+
+      // Validate the expiration:
+      if (!Stripe.card.validateExpiry(expMonth, expYear)) {
+          error = true;
+          reportError('The expiration date appears to be invalid.');
+      }
+
+          if (!error) {
+        // Get the Stripe token:
+        Stripe.card.createToken({
+            number: ccNum,
+            cvc: cvcNum,
+            exp_month: expMonth,
+            exp_year: expYear
+        }, stripeResponseHandler);
+     }
+
+     function stripeResponseHandler(status, response) {
+       console.log('in stripe response handler');
+     }
+
+    if (response.error) {
+        reportError(response.error.message);
+      } else { // No errors, submit the form.
+    }
+
 
     //ensure that shipping copies to billing
     //ensure that when uncheck billing is left blank
@@ -47,6 +96,7 @@ $(document).on('ready', function() {
     // - make sure that expiration date fits the convention of "MM/YYYY" - for reasons of convenience - years will be four digits and not two.
     // - make sure that that the expiration date is equal or greater than today's date
 
+    });
   });
 
   function buildStatesSelector() {
@@ -74,7 +124,7 @@ $(document).on('ready', function() {
     $('#shoppingCartTable').append('<tbody>');
     $('#shoppingCartTable').append('</tbody>');
 
-    
+
   }
 
   // <tbody>
